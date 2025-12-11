abf203e1ff0955ae2e047a9540ed8b9c1d8b6166
shams220 <103259015+shams220@users.noreply.github.com>
2025-12-11 08:55:55 +0530

    Done

--- src/components/tooltip.js
+++ src/components/tooltip.js
@@ -1,21 +1,22 @@
-import React,{useState} from 'react'
-import './tooltip.css'
+import React, { useState } from "react";
+import "./tooltip.css";
 
-const Tooltip = ({text,children}) => {
-  const [visible,setVisible] = useState(false);
-  
-  
+const Tooltip = ({ text, children }) => {
+  const [visible, setVisible] = useState(false);
 
-    return (
-   <div className="tooltip"
-      onMouseEnter={()=>setVisible(true)}
-      onMouseLeave={()=>{setVisible(false)}}
-   >
-{children}
-{visible && <Span className="tooltiptext">{text}</Span> }
-   </div>
-    
-  )
-}
+  return (
+    <div
+      className="tooltip"
+      onMouseEnter={() => setVisible(true)}
+      onMouseLeave={() => setVisible(false)}
+    >
+      {children}
 
-export default Tooltip
+      <span className={`tooltiptext ${visible ? "show" : ""}`}>
+        {text}
+      </span>
+    </div>
+  );
+};
+
+export default Tooltip;
